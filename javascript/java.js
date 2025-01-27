304d4acad20db6deb4381fbf63d5c6a2b0453788
BexCdz <beckypearce92@hotmail.com>
2025-01-27 21:08:44 +0100

    Injected current time and date importing moment and added JavaScript

--- javascript/java.js
+++ javascript/java.js
@@ -1,1 +1,19 @@
+setInterval(function () {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAnglelesDateElement = losAngelesElement.querySelector(".date");
+  let losAnglelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  losAnglelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAnglelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
 
+setInterval(function () {
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}, 1000);
