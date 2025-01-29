51284f2d9e512b64538d6f67ed3c9ed88ef5c295
BexCdz <beckypearce92@hotmail.com>
2025-01-29 20:35:52 +0100

    added link to all cities

--- javascript/java.js
+++ javascript/java.js
@@ -32,7 +32,8 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `<div class="city">
+  citiesElement.innerHTML = `
+  <div class="city">
           <div>
             <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
@@ -41,7 +42,7 @@ function updateCity(event) {
             "h:mm:ss"
           )} <small>${cityTime.format("A")}</small></div>
         </div>
-      </div>`;
+    <a href="/">All cities</a>`;
 }
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
